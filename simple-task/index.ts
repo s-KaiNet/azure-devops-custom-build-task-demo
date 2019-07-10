25f292e71da438ca7b627c074b61f045d1eaf5e0
Sergei Sergeev <sergeev.srg@gmail.com>
2019-07-10 11:44:06 +0300

    Added input.

--- simple-task/index.ts
+++ simple-task/index.ts
@@ -3,4 +3,8 @@ import * as task from "azure-pipelines-task-lib/task";
 task.debug(`Running on nodejs ${process.version}`); // visible only if system.debug is true
 console.log(`Running on nodejs ${process.version}`);
 
+const message = task.getInput('message');
+
+console.log(`Your message: ${message}`);
+
 task.setResult(task.TaskResult.Succeeded, '');
