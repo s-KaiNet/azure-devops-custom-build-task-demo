f70bd326b1b0de4e718ec726b2732ced8d8984fa
Sergei Sergeev <sergeev.srg@gmail.com>
2019-07-10 11:50:27 +0300

    Removed debug.

--- simple-task/index.ts
+++ simple-task/index.ts
@@ -1,6 +1,5 @@
 import * as task from "azure-pipelines-task-lib/task";
 
-task.debug(`Running on nodejs ${process.version}`); // visible only if system.debug is true
 console.log(`Running on nodejs ${process.version}`);
 
 task.setResult(task.TaskResult.Succeeded, '');
